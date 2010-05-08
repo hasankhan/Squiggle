5b60b532f73ab1970e7302926646c1e4d8db3e58
faysalmalik <none@none.com>
2010-05-08 08:01:08 +0000

    Initial Prototype of Squiggle messenger UI.

---
+++ Squiggle.UI/ChatWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Messenger
+{
+    /// <summary>
+    /// Interaction logic for ChatWindow.xaml
+    /// </summary>
+    public partial class ChatWindow : Window
+    {
+        public ChatWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var title = new Bold(new Run("Someone Says: "));
+            var text = new Run(txtMessage.Text);
+            sentMessages.Inlines.Add(title);
+            sentMessages.Inlines.Add(text);
+            sentMessages.Inlines.Add(new Run("\r\n"));
+            scrollViewer.ScrollToBottom();
+            txtMessage.Text = String.Empty;
+            txtMessage.Focus();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ Squiggle.UI/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Messenger
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<User> users = new List<User>()
+            {
+                new User(){DisplayName="Faisal", Status=Status.Online},
+                new User(){DisplayName="Ata", Status=Status.Idle},
+                new User(){DisplayName="Hasan", Status=Status.Offline}
+            };
+
+            this.Users = users;
+            this.DataContext = this;
+        }
+
+        public List<User> Users { get; set; }
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            string displayName = ((TextBlock)sender).Tag.ToString();
+            ChatWindow window = new ChatWindow();
+            window.Title = displayName;
+            window.Show();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //var title = new Bold(new Run("Kashif"));
+            //var status = new Run(" (Online)");
+            //var displayMessage = new Run("You display message goes here...");
+            //txtLoggedInUser.Inlines.Add(title);
+            //txtLoggedInUser.Inlines.Add(status);
+            //txtLoggedInUser.Inlines.Add(Environment.NewLine);
+            //txtLoggedInUser.Inlines.Add(displayMessage);
+        }
+    }
+
+    public class User
+    {
+        public string DisplayName { get; set; }
+        public Status Status { get; set; }
+    }
+
+    public enum Status
+    {
+        Online,
+        Offline,
+        Idle
+    }
+}
