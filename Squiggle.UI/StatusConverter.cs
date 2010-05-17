eb2884357c8669355481eb0feddc809193efbe9a
hasankhan <none@none.com>
2010-05-17 19:56:32 +0000

    now showing better textual representation of status

---
+++ Squiggle.UI/StatusConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using Squiggle.Chat;
+using System.Windows;
+
+namespace Squiggle.UI
+{
+    public class StatusConverter: IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value != null)
+            {
+                var status = (UserStatus)value;
+                switch (status)
+                {
+                    case UserStatus.OnPhone:
+                        return "On Phone";
+                    case UserStatus.InAMeeting:
+                        return "In a Metting";
+                    case UserStatus.OutForLunch:
+                        return "Out for Lunch";
+                    case UserStatus.BeRightBack:
+                        return "Be Right Back";
+                    default:
+                        return status.ToString();
+                }
+            }
+            else
+                return String.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+
+        #endregion
+    }
+}
