558f05df895b7ba8cb595af2e61023854d72562f
faysalmalik <none@none.com>
2010-05-27 17:40:21 +0000

    DisplayMessageConverter.cs file added

---
+++ Squiggle.UI/DisplayMessageConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace Squiggle.UI
+{
+    class DisplayMessageConverter : IValueConverter
+    {
+
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string message = value as string;
+            if (String.IsNullOrEmpty(message))
+                return "Share a quick message here.";
+
+            return message;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
