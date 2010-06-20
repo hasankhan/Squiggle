56eb698adf1dcf51be68e515d1219ee37a7bcfa7
hasankhan <none@none.com>
2010-06-20 18:35:13 +0000

    no need for routedui commands. just gave 'SignedInMenu' style to the send message and file menus

--- Squiggle.UI/MainWindow.xaml.cs
+++ Squiggle.UI/MainWindow.xaml.cs
@@ -29,9 +29,6 @@ namespace Squiggle.UI
         public static MainWindow Instance { get; private set; }
         public IChatClient ChatClient { get; private set; }
 
-        public static RoutedUICommand SendInstantMessageToContacts = new RoutedUICommand("SendInstantMessage", "SendInstantMessage", typeof(MainWindow));
-        public static RoutedUICommand SendFileToContacts = new RoutedUICommand("SendFile", "SendFile", typeof(MainWindow));
-
         bool exiting;
 
         public MainWindow()
@@ -349,30 +346,18 @@ namespace Squiggle.UI
             SquiggleUtility.ShowAboutDialog();
         }
 
-        private void SendInstantMessage_Executed(object sender, ExecutedRoutedEventArgs e)
-        {
-            IEnumerable<Buddy> selectedBuddies = SquiggleUtility.ShowSendInstantMessageDialog(clientViewModel);
-            foreach (Buddy buddy in selectedBuddies)
-                StartChat(buddy);
-        }
-
-        private void SendInstantMessage_CanExecute(object sender, CanExecuteRoutedEventArgs e)
-        {
-            e.CanExecute = clientViewModel != null;
-        }
-
-        private void SendFile_Executed(object sender, ExecutedRoutedEventArgs e)
+        private void SendFileMenu_Click(object sender, RoutedEventArgs e)
         {
             IEnumerable<Buddy> selectedBuddies = SquiggleUtility.ShowSendInstantMessageDialog(clientViewModel);
             foreach (Buddy buddy in selectedBuddies)
                 StartChat(buddy, true, null);
         }
 
-        private void SendFile_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        private void SendMessageMenu_Click(object sender, RoutedEventArgs e)
         {
-            e.CanExecute = clientViewModel != null;
+            IEnumerable<Buddy> selectedBuddies = SquiggleUtility.ShowSendInstantMessageDialog(clientViewModel);
+            foreach (Buddy buddy in selectedBuddies)
+                StartChat(buddy);
         }
-
-
     }
 }
