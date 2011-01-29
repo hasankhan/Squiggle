096f7e80497d1a027c7d36d09e3caac06365c7f8
hasankhan <none@none.com>
2011-01-29 15:47:27 +0000

    fixed the bug with textblock righttoleft punctuation mark retardedness

---
+++ Squiggle.UI/Controls/TextBlockBugFix.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Documents;
+using System.ComponentModel;
+
+namespace Squiggle.UI.Controls
+{
+    public class TextBlockBugFix
+    {
+        public static readonly DependencyProperty Text2Property = DependencyProperty.RegisterAttached("Text2", typeof(string), typeof(TextBlockBugFix), new FrameworkPropertyMetadata(OnTextPropertyChanged));
+
+        public static void SetText2(UIElement element, string value)
+        {           
+            element.SetValue(Text2Property, value);
+        }
+
+        public static string GetText2(UIElement element)
+        {
+            return (string)element.GetValue(Text2Property);
+        }
+
+        static void OnTextPropertyChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            var label = (TextBlock)sender;
+            //http://stackoverflow.com/questions/4835920/round-brackets-not-showing-up-correctly-in-rightoleft-flow-direction-in-wpf/4837069#4837069
+            var bugFixRun = new Run("i") { FontSize = .01 };
+            var textRun = new Run(e.NewValue as string);
+            label.Inlines.Add(textRun);
+            label.Inlines.Add(bugFixRun);
+        }
+    }
+}
