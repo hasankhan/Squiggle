7a5c5e8310b2e6165da8192e0c809664b6f3f185
hasankhan <none@none.com>
2011-01-29 06:20:17 +0000

    Enable audio alerts by default

--- Squiggle.UI/Properties/Settings.Designer.cs
+++ Squiggle.UI/Properties/Settings.Designer.cs
@@ -360,7 +360,7 @@ namespace Squiggle.UI.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool AudioAlerts {
             get {
                 return ((bool)(this["AudioAlerts"]));
