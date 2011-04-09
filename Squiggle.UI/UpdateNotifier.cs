f6ae3c6bc98dc4952f357af92f28f9b361f63159
hasankhan <none@none.com>
2011-04-09 14:41:41 +0000

    update notifier should check creation time because modified time seems to be preserved from my machine

--- Squiggle.UI/UpdateNotifier.cs
+++ Squiggle.UI/UpdateNotifier.cs
@@ -29,7 +29,7 @@ namespace Squiggle.UI
                 var fileInfo = new FileInfo(Assembly.GetExecutingAssembly().Location);
                 if (fileInfo.Exists)
                 {
-                    DateTime fileTime = fileInfo.LastWriteTimeUtc;
+                    DateTime fileTime = fileInfo.CreationTimeUtc;
                     SyndicationItem lastUpdate = GetLastUpdate();
                     if (lastUpdate != null && lastUpdate.PublishDate.UtcDateTime>fileTime)
                     {
