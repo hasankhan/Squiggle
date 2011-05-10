591e26bfdf259c9c53d9ed00b58d92568f69d6b4
hasankhan <none@none.com>
2011-05-10 15:56:41 +0000

    changed version to 2.7.4

--- Shared/AssemblyInfo.Version.cs
+++ Shared/AssemblyInfo.Version.cs
@@ -2,5 +2,5 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("2.7.3.0")]
-[assembly: AssemblyFileVersion("2.7.3.0")]
+[assembly: AssemblyVersion("2.7.4.0")]
+[assembly: AssemblyFileVersion("2.7.4.0")]
