0ed123d9d08d89e0273828643bfb27196438ebeb
hasankhan <none@none.com>
2011-06-19 02:47:11 +0000

    changed version to 3.0

--- Shared/AssemblyInfo.Version.cs
+++ Shared/AssemblyInfo.Version.cs
@@ -2,5 +2,5 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("2.7.5.0")]
-[assembly: AssemblyFileVersion("2.7.5.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
