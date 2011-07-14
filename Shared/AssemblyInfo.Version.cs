a1aec963adc579ce36d4043d3d87729bdce66a49
hasankhan <none@none.com>
2011-07-14 15:39:07 +0000

    Changed version to revision 3.0.1

--- Shared/AssemblyInfo.Version.cs
+++ Shared/AssemblyInfo.Version.cs
@@ -2,5 +2,5 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("3.0.1.0")]
+[assembly: AssemblyFileVersion("3.0.1.0")]
