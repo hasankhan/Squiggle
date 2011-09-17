df4ec8e3efda5fd0a4e82bccaf62a6317efec472
hasankhan <none@none.com>
2011-09-17 11:35:32 +0000

    changed version to 3.0.8 and created new translation files

--- Shared/AssemblyInfo.Version.cs
+++ Shared/AssemblyInfo.Version.cs
@@ -2,5 +2,5 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("3.0.7.0")]
-[assembly: AssemblyFileVersion("3.0.7.0")]
+[assembly: AssemblyVersion("3.0.8.0")]
+[assembly: AssemblyFileVersion("3.0.8.0")]
