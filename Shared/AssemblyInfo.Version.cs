732b1402dd84547423bb659971dbb2d28917f2a6
hasankhan <none@none.com>
2012-06-21 20:54:41 +0000

    changed version to 3.1.6

--- Shared/AssemblyInfo.Version.cs
+++ Shared/AssemblyInfo.Version.cs
@@ -2,5 +2,5 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("3.1.5.0")]
-[assembly: AssemblyFileVersion("3.1.5.0")]
+[assembly: AssemblyVersion("3.1.6.0")]
+[assembly: AssemblyFileVersion("3.1.6.0")]
